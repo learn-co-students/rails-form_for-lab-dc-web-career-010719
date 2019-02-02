a12c0ab35efc5a80e33edac80d85734461c65835
github username <github email address>
2019-02-02 11:38:40 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  resources :student_classes, except: :destroy
+
+  # get 'school_classes/index'
+  #
+  # get 'school_classes/show'
+  #
+  # get 'school_classes/new'
+  #
+  # get 'school_classes/create'
+  #
+  # get 'school_classes/edit'
+  #
+  # get 'school_classes/update'
+  #
+  # get 'students/index'
+  #
+  # get 'students/show'
+  #
+  # get 'students/new'
+  #
+  # get 'students/create'
+  #
+  # get 'students/edit'
+  #
+  # get 'students/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
